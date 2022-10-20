19ec1acaa41d14de94319a8c208a0a86551dab2e
Sri Vignesh <sselvan@redhat.com>
2022-10-20 12:32:58 +0530

    feat(HACBS-1285): add unit testing for gitops binding function
    Signed-off-by: Sri Vignesh <sselvan@redhat.com>

---
+++ gitops/gitops_test.go
@@ -0,0 +1,173 @@
+package gitops_test
+
+import (
+	"fmt"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"time"
+
+	applicationapiv1alpha1 "github.com/redhat-appstudio/application-api/api/v1alpha1"
+	"github.com/redhat-appstudio/integration-service/gitops"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var _ = Describe("Gitops functions for managing ApplicationSnapshots", Ordered, func() {
+
+	var (
+		hasApp      *applicationapiv1alpha1.Application
+		hasComp     *applicationapiv1alpha1.Component
+		hasSnapshot *applicationapiv1alpha1.ApplicationSnapshot
+		env         applicationapiv1alpha1.Environment
+		sampleImage string
+	)
+	const (
+		SampleRepoLink  = "https://github.com/devfile-samples/devfile-sample-java-springboot-basic"
+		namespace       = "default"
+		applicationName = "application-sample"
+		componentName   = "component-sample"
+		snapshotName    = "snapshot-sample"
+	)
+
+	BeforeAll(func() {
+		hasApp = &applicationapiv1alpha1.Application{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      applicationName,
+				Namespace: namespace,
+			},
+			Spec: applicationapiv1alpha1.ApplicationSpec{
+				DisplayName: "application-sample",
+				Description: "This is an example application",
+			},
+		}
+		Expect(k8sClient.Create(ctx, hasApp)).Should(Succeed())
+
+		hasComp = &applicationapiv1alpha1.Component{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      componentName,
+				Namespace: namespace,
+			},
+			Spec: applicationapiv1alpha1.ComponentSpec{
+				ComponentName:  componentName,
+				Application:    applicationName,
+				ContainerImage: "",
+				Source: applicationapiv1alpha1.ComponentSource{
+					ComponentSourceUnion: applicationapiv1alpha1.ComponentSourceUnion{
+						GitSource: &applicationapiv1alpha1.GitSource{
+							URL: SampleRepoLink,
+						},
+					},
+				},
+			},
+		}
+		Expect(k8sClient.Create(ctx, hasComp)).Should(Succeed())
+
+		env = applicationapiv1alpha1.Environment{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "envname",
+				Namespace: "default",
+			},
+			Spec: applicationapiv1alpha1.EnvironmentSpec{
+				Type:               "POC",
+				DisplayName:        "my-environment",
+				DeploymentStrategy: applicationapiv1alpha1.DeploymentStrategy_Manual,
+				ParentEnvironment:  "",
+				Tags:               []string{},
+				Configuration: applicationapiv1alpha1.EnvironmentConfiguration{
+					Env: []applicationapiv1alpha1.EnvVarPair{
+						{
+							Name:  "var_name",
+							Value: "test",
+						},
+					},
+				},
+			},
+		}
+		Expect(k8sClient.Create(ctx, &env)).Should(Succeed())
+	})
+
+	BeforeEach(func() {
+		sampleImage = "quay.io/redhat-appstudio/sample-image:latest"
+
+		hasSnapshot = &applicationapiv1alpha1.ApplicationSnapshot{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      snapshotName,
+				Namespace: namespace,
+				Labels: map[string]string{
+					gitops.ApplicationSnapshotTypeLabel:      gitops.ApplicationSnapshotComponentType,
+					gitops.ApplicationSnapshotComponentLabel: componentName,
+				},
+			},
+			Spec: applicationapiv1alpha1.ApplicationSnapshotSpec{
+				Application: hasApp.Name,
+				Components: []applicationapiv1alpha1.ApplicationSnapshotComponent{
+					{
+						Name:           componentName,
+						ContainerImage: sampleImage,
+					},
+				},
+			},
+		}
+		Expect(k8sClient.Create(ctx, hasSnapshot)).Should(Succeed())
+
+		Eventually(func() error {
+			err := k8sClient.Get(ctx, types.NamespacedName{
+				Name:      hasSnapshot.Name,
+				Namespace: namespace,
+			}, hasSnapshot)
+			return err
+		}, time.Second*10).ShouldNot(HaveOccurred())
+	})
+
+	AfterEach(func() {
+		err := k8sClient.Delete(ctx, hasSnapshot)
+		Expect(err == nil || errors.IsNotFound(err)).To(BeTrue())
+	})
+
+	AfterAll(func() {
+		err := k8sClient.Delete(ctx, hasApp)
+		Expect(err == nil || errors.IsNotFound(err)).To(BeTrue())
+		err = k8sClient.Delete(ctx, hasComp)
+		Expect(err == nil || errors.IsNotFound(err)).To(BeTrue())
+		err = k8sClient.Delete(ctx, &env)
+		Expect(err == nil || errors.IsNotFound(err)).To(BeTrue())
+
+	})
+
+	It("ensures applicationsnapshot environmentBinding doesn't exist", func() {
+		gitops.MarkSnapshotAsPassed(k8sClient, ctx, hasSnapshot, "test passed")
+		Expect(gitops.HaveHACBSTestsSucceeded(hasSnapshot)).To(BeTrue())
+
+		Eventually(func() bool {
+			applicationSnapshotEnvironmentBinding, err := gitops.FindExistingApplicationSnapshotEnvironmentBinding(k8sClient, ctx, hasApp, &env)
+			fmt.Fprintf(GinkgoWriter, "Err: %v\n", err)
+			fmt.Fprintf(GinkgoWriter, "appenvbinding: %v\n", applicationSnapshotEnvironmentBinding)
+			return applicationSnapshotEnvironmentBinding == nil && err == nil
+		}, time.Second*10).Should(BeTrue())
+	})
+
+	It("ensures Binding Component is created", func() {
+		gitops.MarkSnapshotAsPassed(k8sClient, ctx, hasSnapshot, "test passed")
+		Expect(gitops.HaveHACBSTestsSucceeded(hasSnapshot)).To(BeTrue())
+		bindingComponents := gitops.CreateBindingComponents([]applicationapiv1alpha1.Component{*hasComp})
+		Expect(bindingComponents != nil).To(BeTrue())
+	})
+
+	It("ensures Application snapshot Environment Binding is created and exists", func() {
+		bindingName := hasApp.Name + "-" + env.Name + "-" + "binding"
+		applicationSnapshotEnvironmentBinding := gitops.CreateApplicationSnapshotEnvironmentBinding(
+			bindingName, hasApp.Namespace, hasApp.Name,
+			env.Name, hasSnapshot, []applicationapiv1alpha1.Component{*hasComp})
+		Expect(applicationSnapshotEnvironmentBinding != nil).To(BeTrue())
+		Expect(k8sClient.Create(ctx, applicationSnapshotEnvironmentBinding)).Should(Succeed())
+		Eventually(func() bool {
+			applicationSnapshotEnvironmentBinding, err := gitops.FindExistingApplicationSnapshotEnvironmentBinding(k8sClient, ctx, hasApp, &env)
+			return applicationSnapshotEnvironmentBinding != nil && err == nil
+		}, time.Second*10).Should(BeTrue())
+		Expect(k8sClient.Delete(ctx, applicationSnapshotEnvironmentBinding)).Should(Succeed())
+	})
+})
