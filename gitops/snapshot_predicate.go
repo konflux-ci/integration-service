c0cb17b714f1df08f958debbeb205428b5214eff
Jing Qi <jinqi@redhat.com>
2023-01-09 11:09:40 +0800

    feat(HACBS-1500): add filter for delete event for snapshot controller
    Signed-off-by: Jing Qi <jinqi@redhat.com>

---
+++ gitops/snapshot_predicate.go
@@ -0,0 +1,25 @@
+package gitops
+
+import (
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+)
+
+// IntegrationSnapshotChangePredicate returns a predicate which filters out all objects except
+// snapshot is deleted.
+func IntegrationSnapshotChangePredicate() predicate.Predicate {
+	return predicate.Funcs{
+		CreateFunc: func(createEvent event.CreateEvent) bool {
+			return true
+		},
+		DeleteFunc: func(deleteEvent event.DeleteEvent) bool {
+			return false
+		},
+		GenericFunc: func(genericEvent event.GenericEvent) bool {
+			return true
+		},
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			return true
+		},
+	}
+}
