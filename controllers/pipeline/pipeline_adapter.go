34eb341093929b7573aa2f6a7a0f0a7d9b228156
Martin Basti <mbasti@redhat.com>
2023-03-30 19:58:12 +0200

    feat(STONEINTG-346): expose build pipeline in snapshot
    This change makes easier to find which build pipeline created a
    snapshot.
    
    Signed-off-by: Martin Basti <mbasti@redhat.com>

--- controllers/pipeline/pipeline_adapter.go
+++ controllers/pipeline/pipeline_adapter.go
@@ -396,6 +396,7 @@ func (a *Adapter) prepareSnapshotForPipelineRun(pipelineRun *tektonv1beta1.Pipel
 	}
 	snapshot.Labels[gitops.SnapshotTypeLabel] = gitops.SnapshotComponentType
 	snapshot.Labels[gitops.SnapshotComponentLabel] = a.component.Name
+	snapshot.Labels[gitops.BuildPipelineRunNameLabel] = pipelineRun.Name
 	if pipelineRun.Status.CompletionTime != nil {
 		snapshot.Labels[gitops.BuildPipelineRunFinishTimeLabel] = strconv.FormatInt(pipelineRun.Status.CompletionTime.Time.Unix(), 10)
 	} else {
