7b6e047809162a6cef1c9a62e6d35c8e4f228b73
Martin Basti <mbasti@redhat.com>
2023-10-06 10:15:51 +0200

    feat(statusreporting): add feature flag
    Adding feature flag FEATURE_STATUS_REPORTING_ENABLED to enable feature
    for reporting tests statuses into pull requestes. Featue is not ready
    yet, thus is disabled by default.
    
    Signed-off-by: Martin Basti <mbasti@redhat.com>

--- controllers/statusreport/statusreport_adapter.go
+++ controllers/statusreport/statusreport_adapter.go
@@ -18,6 +18,7 @@ package statusreport
 
 import (
 	"context"
+	"os"
 
 	applicationapiv1alpha1 "github.com/redhat-appstudio/application-api/api/v1alpha1"
 	"github.com/redhat-appstudio/integration-service/helpers"
@@ -27,6 +28,8 @@ import (
 	"github.com/redhat-appstudio/operator-toolkit/controller"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
+
+const FeatureFlagStatusReprotingEnabled = "FEATURE_STATUS_REPORTING_ENABLED"
 
 // Adapter holds the objects needed to reconcile a snapshot's test status report.
 type Adapter struct {
@@ -56,6 +59,10 @@ func NewAdapter(snapshot *applicationapiv1alpha1.Snapshot, application *applicat
 // EnsureSnapshotTestStatusReported will ensure that integration test status including env provision and snapshotEnvironmentBinding error is reported to the git provider
 // which (indirectly) triggered its execution.
 func (a *Adapter) EnsureSnapshotTestStatusReported() (controller.OperationResult, error) {
+	if !isFeatureEnabled() {
+		return controller.ContinueProcessing()
+	}
+
 	reporters, err := a.status.GetReporters(a.snapshot)
 	if err != nil {
 		return controller.RequeueWithError(err)
@@ -70,4 +77,12 @@ func (a *Adapter) EnsureSnapshotTestStatusReported() (controller.OperationResult
 	}
 
 	return controller.ContinueProcessing()
+}
+
+// isFeatureEnabled returns true when the feature flag FEATURE_STATUS_REPORTING_ENABLED has been defined in env vars
+func isFeatureEnabled() bool {
+	if _, found := os.LookupEnv(FeatureFlagStatusReprotingEnabled); found {
+		return true
+	}
+	return false
 }
