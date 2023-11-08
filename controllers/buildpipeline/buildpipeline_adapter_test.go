e3e6b399491ede6125a8033c3a1cb4d97061bf3a
dirgim <kpavic@redhat.com>
2023-11-08 11:55:13 +0100

    feat(STONEINTG-619): remove loader/integration.go
    * The functions within loader/integration.go
      are not used in the integration-service any more
      and are causing unneeded churn for updating to
      Tekton v1 APIs
    
    Signed-off-by: dirgim <kpavic@redhat.com>

--- controllers/buildpipeline/buildpipeline_adapter_test.go
+++ controllers/buildpipeline/buildpipeline_adapter_test.go
@@ -36,8 +36,8 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 
 	applicationapiv1alpha1 "github.com/redhat-appstudio/application-api/api/v1alpha1"
-	tektonv1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1"
 	toolkit "github.com/redhat-appstudio/operator-toolkit/loader"
+	tektonv1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1"
 	"github.com/tonglil/buflogr"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
