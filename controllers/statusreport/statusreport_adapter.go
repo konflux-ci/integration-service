konflux-ci/integration-service

commit bc758969f9ec196d57002c3abe2ea130e108bcd3

refactor(STONEINTG-769): prepare for new reporter
Refactor code to allow to add new reporter easier.

Changes:
* `Status` is responsible to decide when and what should be reported
  into git providers.
* Status is also responsible to fetch test results and generate markdown
  output instead of reporter. Reporter will just report it. In case of
need this can be updated to templating in future, but reporter shouldn't
be responsible for fetching task results.
* `Reporter` is responsible to only pass data in correct format, and map
  values to values accepted by git provider (for example status value)
* Only one reporter is returned, we cannot support multiple reporters
  (PR/MR is always from the single provider)
* Interfaces and mocking for unittesting implemented.

Signed-off-by: Martin Basti <mbasti@redhat.com>

controllers/statusreport/statusreport_adapter.go
@@ -43,7 +43,7 @@ type Adapter struct {
 	loader      loader.ObjectLoader
 	client      client.Client
 	context     context.Context
-	status      status.Status
+	status      status.StatusInterface
 }
 
 // NewAdapter creates and returns an Adapter instance.
@@ -56,7 +56,7 @@ func NewAdapter(snapshot *applicationapiv1alpha1.Snapshot, application *applicat
 		loader:      loader,
 		client:      client,
 		context:     context,
-		status:      status.NewAdapter(logger.Logger, client),
+		status:      status.NewStatus(logger.Logger, client),
 	}
 }
 
@@ -67,17 +67,16 @@ func (a *Adapter) EnsureSnapshotTestStatusReportedToGitHub() (controller.Operati
 		return controller.ContinueProcessing()
 	}
 
-	reporters, err := a.status.GetReporters(a.snapshot)
-	if err != nil {
-		return controller.RequeueWithError(err)
+	reporter := a.status.GetReporter(a.snapshot)
+	if reporter == nil {
+		a.logger.Info("No suitable reporter found, skipping report")
+		return controller.ContinueProcessing()
 	}
+	a.logger.Info(fmt.Sprintf("Detected reporter: %s", reporter.GetReporterName()))
 
-	for _, reporter := range reporters {
-		if err := reporter.ReportStatusForSnapshot(a.client, a.context, &a.logger, a.snapshot); err != nil {
-			a.logger.Error(err, "failed to report test status to github for snapshot",
-				"snapshot.Namespace", a.snapshot.Namespace, "snapshot.Name", a.snapshot.Name)
-			return controller.RequeueWithError(err)
-		}
+	err := a.status.ReportSnapshotStatus(a.context, reporter, a.snapshot)
+	if err != nil {
+		return controller.RequeueWithError(fmt.Errorf("failed to report status: %w", err))
 	}
 
 	if gitops.IsSnapshotMarkedAsPassed(a.snapshot) || gitops.IsSnapshotMarkedAsFailed(a.snapshot) || gitops.IsSnapshotMarkedAsInvalid(a.snapshot) {

status/status.go
@@ -16,70 +16,190 @@ limitations under the License.
 
 package status
 
+//go:generate mockgen -destination mock_status.go -package status github.com/redhat-appstudio/integration-service/status StatusInterface
+
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"github.com/go-logr/logr"
 	applicationapiv1alpha1 "github.com/redhat-appstudio/application-api/api/v1alpha1"
 	"github.com/redhat-appstudio/integration-service/gitops"
 	"github.com/redhat-appstudio/integration-service/helpers"
-	"github.com/redhat-appstudio/operator-toolkit/metadata"
+	intgteststat "github.com/redhat-appstudio/integration-service/pkg/integrationteststatus"
+	tektonv1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // NamePrefix is a common name prefix for this service.
 const NamePrefix = "Red Hat Trusted App Test"
 
-// Reporter is a generic interface all status implementations must follow.
-type Reporter interface {
-	ReportStatusForSnapshot(client.Client, context.Context, *helpers.IntegrationLogger, *applicationapiv1alpha1.Snapshot) error
+// generate TestReport to be used by all reporters
+func generateTestReport(ctx context.Context, client client.Client, detail intgteststat.IntegrationTestStatusDetail, snapshot *applicationapiv1alpha1.Snapshot) (*TestReport, error) {
+	text, err := generateText(client, ctx, detail, snapshot.Namespace)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate text message: %w", err)
+	}
+
+	summary, err := generateSummary(detail.Status, snapshot.Name, detail.ScenarioName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate summary message: %w", err)
+	}
+
+	report := TestReport{
+		Text:           text,
+		FullName:       NamePrefix + " / " + snapshot.Name + " / " + detail.ScenarioName,
+		ScenarioName:   detail.ScenarioName,
+		Status:         detail.Status,
+		Summary:        summary,
+		StartTime:      detail.StartTime,
+		CompletionTime: detail.CompletionTime,
+	}
+	return &report, nil
 }
 
-// Status is the interface of the main status Adapter.
-type Status interface {
-	GetReporters(client.Object) ([]Reporter, error)
+type StatusInterface interface {
+	GetReporter(*applicationapiv1alpha1.Snapshot) ReporterInterface
+	ReportSnapshotStatus(context.Context, ReporterInterface, *applicationapiv1alpha1.Snapshot) error
 }
 
-// Adapter is responsible for discovering supported Reporter implementations.
-type Adapter struct {
-	logger         logr.Logger
-	k8sClient      client.Reader
-	githubReporter Reporter
+type Status struct {
+	logger logr.Logger
+	client client.Client
 }
 
-// AdapterOption is used to extend Adapter with optional parameters.
-type AdapterOption = func(a *Adapter)
+// check if interface has been implemented correctly
+var _ StatusInterface = (*Status)(nil)
 
-// WithGitHubReporter is an option which allows for replacement of the GitHub PipelineRun reporter.
-func WithGitHubReporter(reporter Reporter) AdapterOption {
-	return func(a *Adapter) {
-		a.githubReporter = reporter
+func NewStatus(logger logr.Logger, client client.Client) *Status {
+	return &Status{
+		logger: logger,
+		client: client,
 	}
 }
 
-// NewAdapter constructs an Adapter with optional params, if specified.
-func NewAdapter(logger logr.Logger, k8sClient client.Client, opts ...AdapterOption) *Adapter {
-	adapter := Adapter{
-		logger:         logger,
-		k8sClient:      k8sClient,
-		githubReporter: NewGitHubReporter(logger, k8sClient),
+// GetReporter returns reporter to process snapshot using the right git provider, nil means no suitable reporter found
+func (s *Status) GetReporter(snapshot *applicationapiv1alpha1.Snapshot) ReporterInterface {
+	githubReporter := NewGitHubReporter(s.logger, s.client)
+	if githubReporter.Detect(snapshot) {
+		return githubReporter
 	}
 
-	for _, opt := range opts {
-		opt(&adapter)
-	}
-
-	return &adapter
+	return nil
 }
 
-// GetReporters returns a list of enabled/supported status reporters for a PipelineRun.
-// All potential reporters must be added to this function for them to be utilized.
-func (a *Adapter) GetReporters(object client.Object) ([]Reporter, error) {
-	var reporters []Reporter
+// ReportSnapshotStatus reports status of all integration tests into Pull Request
+func (s *Status) ReportSnapshotStatus(ctx context.Context, reporter ReporterInterface, snapshot *applicationapiv1alpha1.Snapshot) error {
 
-	if metadata.HasLabelWithValue(object, gitops.PipelineAsCodeGitProviderLabel, gitops.PipelineAsCodeGitHubProviderType) {
-		reporters = append(reporters, a.githubReporter)
+	statuses, err := gitops.NewSnapshotIntegrationTestStatusesFromSnapshot(snapshot)
+	if err != nil {
+		s.logger.Error(err, "failed to get test status annotations from snapshot",
+			"snapshot.Namespace", snapshot.Namespace, "snapshot.Name", snapshot.Name)
+		return err
 	}
 
-	return reporters, nil
+	integrationTestStatusDetails := statuses.GetStatuses()
+	if len(integrationTestStatusDetails) == 0 {
+		// no tests to report, skip
+		s.logger.Info("No test result to report to GitHub, skipping",
+			"snapshot.Namespace", snapshot.Namespace, "snapshot.Name", snapshot.Name)
+		return nil
+	}
+
+	if err := reporter.Initialize(ctx, snapshot); err != nil {
+		s.logger.Error(err, "Failed to initialize reporter", "reporter", reporter.GetReporterName())
+		return fmt.Errorf("failed to initialize reporter: %w", err)
+	}
+	s.logger.Info("Reporter initialized", "reporter", reporter.GetReporterName())
+
+	latestUpdateTime, err := gitops.GetLatestUpdateTime(snapshot)
+	if err != nil {
+		s.logger.Error(err, "failed to get latest update annotation for snapshot",
+			"snapshot.NameSpace", snapshot.Namespace, "snapshot.Name", snapshot.Name)
+		latestUpdateTime = time.Time{}
+	}
+	newLatestUpdateTime := latestUpdateTime
+
+	for _, integrationTestStatusDetail := range integrationTestStatusDetails {
+		if latestUpdateTime.Before(integrationTestStatusDetail.LastUpdateTime) {
+			s.logger.Info("Integration Test contains new status updates", "scenario.Name", integrationTestStatusDetail.ScenarioName)
+			if newLatestUpdateTime.Before(integrationTestStatusDetail.LastUpdateTime) {
+				newLatestUpdateTime = integrationTestStatusDetail.LastUpdateTime
+			}
+		} else {
+			//integration test contains no changes
+			continue
+		}
+		testReport, err := generateTestReport(ctx, s.client, *integrationTestStatusDetail, snapshot)
+		if err != nil {
+			return fmt.Errorf("failed to generate test report: %w", err)
+		}
+		if err := reporter.ReportStatus(ctx, *testReport); err != nil {
+			return fmt.Errorf("failed to update status: %w", err)
+		}
+
+	}
+	_ = gitops.SetLatestUpdateTime(snapshot, newLatestUpdateTime)
+	return nil
+}
+
+// generateText generates a text with details for the given state
+func generateText(k8sClient client.Client, ctx context.Context, integrationTestStatusDetail intgteststat.IntegrationTestStatusDetail, namespace string) (string, error) {
+	if integrationTestStatusDetail.Status == intgteststat.IntegrationTestStatusTestPassed || integrationTestStatusDetail.Status == intgteststat.IntegrationTestStatusTestFail {
+		pipelineRunName := integrationTestStatusDetail.TestPipelineRunName
+		pipelineRun := &tektonv1.PipelineRun{}
+		err := k8sClient.Get(ctx, types.NamespacedName{
+			Namespace: namespace,
+			Name:      pipelineRunName,
+		}, pipelineRun)
+		if err != nil {
+			return "", fmt.Errorf("error while getting the pipelineRun %s: %w", pipelineRunName, err)
+		}
+
+		taskRuns, err := helpers.GetAllChildTaskRunsForPipelineRun(k8sClient, ctx, pipelineRun)
+		if err != nil {
+			return "", fmt.Errorf("error while getting all child taskRuns from pipelineRun %s: %w", pipelineRunName, err)
+		}
+		text, err := FormatTestsSummary(taskRuns)
+		if err != nil {
+			return "", err
+		}
+		return text, nil
+	} else {
+		text := integrationTestStatusDetail.Details
+		return text, nil
+	}
+}
+
+// generateSummary returns summary for the given state, snapshotName and scenarioName
+func generateSummary(state intgteststat.IntegrationTestStatus, snapshotName, scenarioName string) (string, error) {
+	var summary string
+	var statusDesc string
+
+	switch state {
+	case intgteststat.IntegrationTestStatusPending:
+		statusDesc = "is pending"
+	case intgteststat.IntegrationTestStatusInProgress:
+		statusDesc = "is in progress"
+	case intgteststat.IntegrationTestStatusEnvironmentProvisionError:
+		statusDesc = "experienced an error when provisioning environment"
+	case intgteststat.IntegrationTestStatusDeploymentError:
+		statusDesc = "experienced an error when deploying snapshotEnvironmentBinding"
+	case intgteststat.IntegrationTestStatusDeleted:
+		statusDesc = "was deleted before the pipelineRun could finish"
+	case intgteststat.IntegrationTestStatusTestPassed:
+		statusDesc = "has passed"
+	case intgteststat.IntegrationTestStatusTestFail:
+		statusDesc = "has failed"
+	case intgteststat.IntegrationTestStatusTestInvalid:
+		statusDesc = "is invalid"
+	default:
+		return summary, fmt.Errorf("unknown status")
+	}
+
+	summary = fmt.Sprintf("Integration test for snapshot %s and scenario %s %s", snapshotName, scenarioName, statusDesc)
+
+	return summary, nil
 }

codecov.yml
@@ -3,6 +3,8 @@ ignore:
   - "api/v1alpha1/zz_generated.deepcopy.go" # generated file
   - "api/v1beta1/zz_generated.deepcopy.go" # generated file
   - "gitops/integrationteststatus_enumer.go" # generated file
+  - "status/mock_reporter.go" # generated file
+  - "status/mock_status.go" # generated file
 
 coverage:
     status:

controllers/integrationpipeline/integrationpipeline_adapter.go
@@ -28,7 +28,6 @@ import (
 	h "github.com/redhat-appstudio/integration-service/helpers"
 	"github.com/redhat-appstudio/integration-service/loader"
 	intgteststat "github.com/redhat-appstudio/integration-service/pkg/integrationteststatus"
-	"github.com/redhat-appstudio/integration-service/status"
 	"github.com/redhat-appstudio/integration-service/tekton"
 
 	"github.com/redhat-appstudio/operator-toolkit/controller"
@@ -46,7 +45,6 @@ type Adapter struct {
 	logger      h.IntegrationLogger
 	client      client.Client
 	context     context.Context
-	status      status.Status
 }
 
 // NewAdapter creates and returns an Adapter instance.
@@ -60,7 +58,6 @@ func NewAdapter(pipelineRun *tektonv1.PipelineRun, application *applicationapiv1
 		loader:      loader,
 		client:      client,
 		context:     context,
-		status:      status.NewAdapter(logger.Logger, client),
 	}
 }
 

controllers/statusreport/statusreport_adapter_test.go
@@ -18,13 +18,13 @@ package statusreport
 
 import (
 	"bytes"
-	"context"
 	"fmt"
 	"reflect"
 	"time"
 
 	"github.com/redhat-appstudio/integration-service/api/v1beta1"
 	"github.com/tonglil/buflogr"
+	"go.uber.org/mock/gomock"
 	"k8s.io/apimachinery/pkg/api/meta"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -44,33 +44,11 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-type MockStatusAdapter struct {
-	Reporter          *MockStatusReporter
-	GetReportersError error
-}
-
-type MockStatusReporter struct {
-	Called            bool
-	ReportStatusError error
-}
-
-func (r *MockStatusReporter) ReportStatusForSnapshot(client.Client, context.Context, *helpers.IntegrationLogger, *applicationapiv1alpha1.Snapshot) error {
-	r.Called = true
-	r.ReportStatusError = nil
-	return r.ReportStatusError
-}
-
-func (a *MockStatusAdapter) GetReporters(object client.Object) ([]status.Reporter, error) {
-	return []status.Reporter{a.Reporter}, a.GetReportersError
-}
-
 var _ = Describe("Snapshot Adapter", Ordered, func() {
 	var (
-		adapter        *Adapter
-		logger         helpers.IntegrationLogger
-		statusAdapter  *MockStatusAdapter
-		statusReporter *MockStatusReporter
-		buf            bytes.Buffer
+		adapter *Adapter
+		logger  helpers.IntegrationLogger
+		buf     bytes.Buffer
 
 		hasComp                 *applicationapiv1alpha1.Component
 		hasComp2                *applicationapiv1alpha1.Component
@@ -270,10 +248,19 @@ var _ = Describe("Snapshot Adapter", Ordered, func() {
 		})
 
 		It("ensures the statusReport is called", func() {
+
+			ctrl := gomock.NewController(GinkgoT())
+			mockReporter := status.NewMockReporterInterface(ctrl)
+			mockStatus := status.NewMockStatusInterface(ctrl)
+
+			mockReporter.EXPECT().GetReporterName().Return("mocked_reporter")
+
+			mockStatus.EXPECT().GetReporter(gomock.Any()).Return(mockReporter)
+			// ReportSnapshotStatus must be called once
+			mockStatus.EXPECT().ReportSnapshotStatus(gomock.Any(), gomock.Any(), gomock.Any()).Times(1)
+
 			adapter = NewAdapter(hasPRSnapshot, hasApp, logger, loader.NewMockLoader(), k8sClient, ctx)
-			statusReporter = &MockStatusReporter{}
-			statusAdapter = &MockStatusAdapter{Reporter: statusReporter}
-			adapter.status = statusAdapter
+			adapter.status = mockStatus
 			adapter.context = toolkit.GetMockedContext(ctx, []toolkit.MockData{
 				{
 					ContextKey: loader.ApplicationContextKey,

go.mod
@@ -18,6 +18,7 @@ require (
 	github.com/santhosh-tekuri/jsonschema/v5 v5.3.1
 	github.com/tektoncd/pipeline v0.56.1
 	github.com/tonglil/buflogr v1.1.1
+	go.uber.org/mock v0.4.0
 	go.uber.org/zap v1.26.0
 	golang.org/x/oauth2 v0.17.0
 	k8s.io/api v0.29.2

go.sum
@@ -355,6 +355,8 @@ go.uber.org/atomic v1.4.0/go.mod h1:gD2HeocX3+yG+ygLZcrzQJaqmWj9AIm7n08wl/qW/PE=
 go.uber.org/atomic v1.10.0 h1:9qC72Qh0+3MqyJbAn8YU5xVq1frD8bn3JtD2oXtafVQ=
 go.uber.org/atomic v1.10.0/go.mod h1:LUxbIzbOniOlMKjJjyPfpl4v+PKK2cNJn91OQbhoJI0=
 go.uber.org/goleak v1.3.0 h1:2K3zAYmnTNqV73imy9J1T3WC+gmCePx2hEGkimedGto=
+go.uber.org/mock v0.4.0 h1:VcM4ZOtdbR4f6VXfiOpwpVJDL6lCReaZ6mw31wqh7KU=
+go.uber.org/mock v0.4.0/go.mod h1:a6FSlNadKUHUa9IP5Vyt1zh4fC7uAwxMutEAscFbkZc=
 go.uber.org/multierr v1.1.0/go.mod h1:wR5kodmAFQ0UK8QlbwjlSNy0Z68gJhDJUG5sjR94q/0=
 go.uber.org/multierr v1.11.0 h1:blXXJkSxSSfBVBlC76pxqeO+LN3aDfLQo+309xJstO0=
 go.uber.org/multierr v1.11.0/go.mod h1:20+QtiLqy0Nd6FdQB9TLXag12DsQkrbs3htMFfDN80Y=

status/format.go
@@ -48,8 +48,8 @@ type CommentTemplateData struct {
 	Summary string
 }
 
-// FormatSummary builds a markdown summary for a list of integration TaskRuns.
-func FormatSummary(taskRuns []*helpers.TaskRun) (string, error) {
+// FormatTestsSummary builds a markdown summary for a list of integration TaskRuns.
+func FormatTestsSummary(taskRuns []*helpers.TaskRun) (string, error) {
 	funcMap := template.FuncMap{
 		"formatTaskName":  FormatTaskName,
 		"formatNamespace": FormatNamespace,
@@ -66,26 +66,10 @@ func FormatSummary(taskRuns []*helpers.TaskRun) (string, error) {
 	return buf.String(), nil
 }
 
-// FormatCommentForFinishedPipelineRun builds a markdown comment for a list of finished integration TaskRuns
-func FormatCommentForFinishedPipelineRun(title string, results []*helpers.TaskRun) (string, error) {
-	summary, err := FormatSummary(results)
-	if err != nil {
-		return "", err
-	}
-
+// FormatComment build a markdown comment with the details in text
+func FormatComment(title, text string) (string, error) {
 	buf := bytes.Buffer{}
-	data := CommentTemplateData{Title: title, Summary: summary}
-	t := template.Must(template.New("").Parse(commentTemplate))
-	if err := t.Execute(&buf, data); err != nil {
-		return "", err
-	}
-	return buf.String(), nil
-}
-
-// FormatCommentForDetail build a markdown comment for the details of unfinished integrationTest
-func FormatCommentForDetail(title, detail string) (string, error) {
-	buf := bytes.Buffer{}
-	data := CommentTemplateData{Title: title, Summary: detail}
+	data := CommentTemplateData{Title: title, Summary: text}
 	t := template.Must(template.New("").Parse(commentTemplate))
 	if err := t.Execute(&buf, data); err != nil {
 		return "", err

commit eb6c32b787a8ba8733d21443d7cb4bdddb51a8c9

fix(STONEINTG-769): do not block reconciliation when report fail
This is regression from bc758969f9ec196d57002c3abe2ea130e108bcd3

Before this status has been ignored at lower level, hotfixing issue.

if PLR doesn't exist anymore, at least provide users some results withou
details, not just failing silently.

ContinueProcessing had to be removed, it would prevent unlocking
finalizers.

This is just workaround, to prevent catastrophic issues on deployments.
Future fix should be about preventing to even get to state where we are
missing PLR data.

Signed-off-by: Martin Basti <mbasti@redhat.com>

controllers/statusreport/statusreport_adapter.go
@@ -83,8 +83,6 @@ func (a *Adapter) EnsureSnapshotTestStatusReportedToGitProvider() (controller.Op
 			"snapshot.Namespace", a.snapshot.Namespace, "snapshot.Name", a.snapshot.Name)
 		if helpers.IsObjectYoungerThanThreshold(a.snapshot, SnapshotRetryTimeout) {
 			return controller.RequeueWithError(err)
-		} else {
-			return controller.ContinueProcessing()
 		}
 	}
 

status/status.go
@@ -29,36 +29,13 @@ import (
 	"github.com/redhat-appstudio/integration-service/helpers"
 	intgteststat "github.com/redhat-appstudio/integration-service/pkg/integrationteststatus"
 	tektonv1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // NamePrefix is a common name prefix for this service.
 const NamePrefix = "Red Hat Trusted App Test"
-
-// generate TestReport to be used by all reporters
-func generateTestReport(ctx context.Context, client client.Client, detail intgteststat.IntegrationTestStatusDetail, snapshot *applicationapiv1alpha1.Snapshot) (*TestReport, error) {
-	text, err := generateText(client, ctx, detail, snapshot.Namespace)
-	if err != nil {
-		return nil, fmt.Errorf("failed to generate text message: %w", err)
-	}
-
-	summary, err := GenerateSummary(detail.Status, snapshot.Name, detail.ScenarioName)
-	if err != nil {
-		return nil, fmt.Errorf("failed to generate summary message: %w", err)
-	}
-
-	report := TestReport{
-		Text:           text,
-		FullName:       NamePrefix + " / " + snapshot.Name + " / " + detail.ScenarioName,
-		ScenarioName:   detail.ScenarioName,
-		Status:         detail.Status,
-		Summary:        summary,
-		StartTime:      detail.StartTime,
-		CompletionTime: detail.CompletionTime,
-	}
-	return &report, nil
-}
 
 type StatusInterface interface {
 	GetReporter(*applicationapiv1alpha1.Snapshot) ReporterInterface
@@ -137,7 +114,7 @@ func (s *Status) ReportSnapshotStatus(ctx context.Context, reporter ReporterInte
 			//integration test contains no changes
 			continue
 		}
-		testReport, err := generateTestReport(ctx, s.client, *integrationTestStatusDetail, snapshot)
+		testReport, err := s.generateTestReport(ctx, *integrationTestStatusDetail, snapshot)
 		if err != nil {
 			return fmt.Errorf("failed to generate test report: %w", err)
 		}
@@ -150,20 +127,51 @@ func (s *Status) ReportSnapshotStatus(ctx context.Context, reporter ReporterInte
 	return nil
 }
 
+// generateTestReport generates TestReport to be used by all reporters
+func (s *Status) generateTestReport(ctx context.Context, detail intgteststat.IntegrationTestStatusDetail, snapshot *applicationapiv1alpha1.Snapshot) (*TestReport, error) {
+	text, err := s.generateText(ctx, detail, snapshot.Namespace)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate text message: %w", err)
+	}
+
+	summary, err := GenerateSummary(detail.Status, snapshot.Name, detail.ScenarioName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate summary message: %w", err)
+	}
+
+	report := TestReport{
+		Text:           text,
+		FullName:       NamePrefix + " / " + snapshot.Name + " / " + detail.ScenarioName,
+		ScenarioName:   detail.ScenarioName,
+		Status:         detail.Status,
+		Summary:        summary,
+		StartTime:      detail.StartTime,
+		CompletionTime: detail.CompletionTime,
+	}
+	return &report, nil
+}
+
 // generateText generates a text with details for the given state
-func generateText(k8sClient client.Client, ctx context.Context, integrationTestStatusDetail intgteststat.IntegrationTestStatusDetail, namespace string) (string, error) {
+func (s *Status) generateText(ctx context.Context, integrationTestStatusDetail intgteststat.IntegrationTestStatusDetail, namespace string) (string, error) {
 	if integrationTestStatusDetail.Status == intgteststat.IntegrationTestStatusTestPassed || integrationTestStatusDetail.Status == intgteststat.IntegrationTestStatusTestFail {
 		pipelineRunName := integrationTestStatusDetail.TestPipelineRunName
 		pipelineRun := &tektonv1.PipelineRun{}
-		err := k8sClient.Get(ctx, types.NamespacedName{
+		err := s.client.Get(ctx, types.NamespacedName{
 			Namespace: namespace,
 			Name:      pipelineRunName,
 		}, pipelineRun)
+
 		if err != nil {
+			if errors.IsNotFound(err) {
+				s.logger.Error(err, "Failed to fetch pipelineRun", "pipelineRun.Name", pipelineRunName)
+				text := fmt.Sprintf("%s\n\n\n(Failed to fetch test result details.)", integrationTestStatusDetail.Details)
+				return text, nil
+			}
+
 			return "", fmt.Errorf("error while getting the pipelineRun %s: %w", pipelineRunName, err)
 		}
 
-		taskRuns, err := helpers.GetAllChildTaskRunsForPipelineRun(k8sClient, ctx, pipelineRun)
+		taskRuns, err := helpers.GetAllChildTaskRunsForPipelineRun(s.client, ctx, pipelineRun)
 		if err != nil {
 			return "", fmt.Errorf("error while getting all child taskRuns from pipelineRun %s: %w", pipelineRunName, err)
 		}