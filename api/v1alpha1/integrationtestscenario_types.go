b01cca3464391341a012e0b4e9e2a11fbd4f36a3
Jiri Sztuka <jsztuka@redhat.com>
2024-05-03 15:56:38 +0200

    fix(STONE-841-pm): serve v1alpha1 back
    Signed-off-by: Jiri Sztuka <jsztuka@redhat.com>
    
    * Conversion.go cannot work without v1alpha1 version
    * Even if its not part of conversion itself

--- api/v1alpha1/integrationtestscenario_types.go
+++ api/v1alpha1/integrationtestscenario_types.go
@@ -66,7 +66,6 @@ type TestContext struct {
 	Description string `json:"description,omitempty"`
 }
 
-// +kubebuilder:unservedversion
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
 // +kubebuilder:printcolumn:name="Application",type=string,JSONPath=`.spec.application`
