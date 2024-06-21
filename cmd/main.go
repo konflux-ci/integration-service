72e1d5918625ce7a04ec360aba2fca3f74fa421e
Hongwei Liu <hongliu@redhat.com>
2024-06-21 11:10:14 +0800

    feat(STONEINTG-955): add metric for intg svc availability
    * check integration service availability by checking the connection with
      github
    
    Signed-off-by: Hongwei Liu <hongliu@redhat.com>

--- cmd/main.go
+++ cmd/main.go
@@ -22,6 +22,8 @@ import (
 	"os"
 
 	"github.com/konflux-ci/integration-service/internal/controller"
+	imetrics "github.com/konflux-ci/integration-service/pkg/metrics"
+	"sigs.k8s.io/controller-runtime/pkg/metrics"
 	"sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	crwebhook "sigs.k8s.io/controller-runtime/pkg/webhook"
 
@@ -130,8 +132,16 @@ func main() {
 		os.Exit(1)
 	}
 
+	ctx := ctrl.SetupSignalHandler()
+	integrationMetrics := imetrics.NewIntegrationMetrics([]imetrics.AvailabilityProbe{imetrics.NewGithubAppAvailabilityProbe(mgr.GetClient())})
+	if err := integrationMetrics.InitMetrics(metrics.Registry); err != nil {
+		setupLog.Error(err, "unable to initialize metrics")
+		os.Exit(1)
+	}
+	integrationMetrics.StartAvailabilityProbes(ctx)
+
 	setupLog.Info("starting manager")
-	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+	if err := mgr.Start(ctx); err != nil {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
 	}
