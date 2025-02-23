2aa50da9e785ca19ea65602cabed5e67429e92bc
red-hat-konflux <126015336+red-hat-konflux[bot]@users.noreply.github.com>
2025-02-23 09:02:37 +0000

    fix(deps): update github.com/konflux-ci/release-service digest to 2f1b5f6
    Signed-off-by: red-hat-konflux <126015336+red-hat-konflux[bot]@users.noreply.github.com>

--- vendor/github.com/konflux-ci/release-service/metadata/labels.go
+++ vendor/github.com/konflux-ci/release-service/metadata/labels.go
@@ -20,47 +20,71 @@ import "fmt"
 
 // Common constants
 const (
-	// rhtapDomain is the prefix of the application label
-	rhtapDomain = "appstudio.openshift.io"
+	// RhtapDomain is the prefix of the application label
+	RhtapDomain = "appstudio.openshift.io"
 
 	// MaxLabelLength is the maximum allowed characters in a label value
 	MaxLabelLength = 63
+
+	// Release service name
+	ServiceName = "release"
+)
+
+// Prefixes used by the release controller package
+var (
+	// PipelinesAsCodePrefix contains the prefix applied to labels and annotations copied from Pipelines as Code resources.
+	PipelinesAsCodePrefix = "pac.test.appstudio.openshift.io"
 )
 
 // Labels used by the release api package
 var (
 	// AttributionLabel is the label name for the standing-attribution label
-	AttributionLabel = fmt.Sprintf("release.%s/standing-attribution", rhtapDomain)
+	AttributionLabel = fmt.Sprintf("release.%s/standing-attribution", RhtapDomain)
 
 	// AutoReleaseLabel is the label name for the auto-release setting
-	AutoReleaseLabel = fmt.Sprintf("release.%s/auto-release", rhtapDomain)
+	AutoReleaseLabel = fmt.Sprintf("release.%s/auto-release", RhtapDomain)
 
 	// AuthorLabel is the label name for the user who creates a CR
-	AuthorLabel = fmt.Sprintf("release.%s/author", rhtapDomain)
+	AuthorLabel = fmt.Sprintf("release.%s/author", RhtapDomain)
 
 	// AutomatedLabel is the label name for marking a Release as automated
-	AutomatedLabel = fmt.Sprintf("release.%s/automated", rhtapDomain)
+	AutomatedLabel = fmt.Sprintf("release.%s/automated", RhtapDomain)
+
+	// ServiceNameLabel is the label used to specify the service associated with an object
+	ServiceNameLabel = fmt.Sprintf("%s/%s", RhtapDomain, "service")
 
 	// ReleasePlanAdmissionLabel is the ReleasePlan label for the name of the ReleasePlanAdmission to use
-	ReleasePlanAdmissionLabel = fmt.Sprintf("release.%s/releasePlanAdmission", rhtapDomain)
+	ReleasePlanAdmissionLabel = fmt.Sprintf("release.%s/releasePlanAdmission", RhtapDomain)
 )
 
 // Prefixes to be used by Release Pipelines labels
 var (
 	// pipelinesLabelPrefix is the prefix of the pipelines label
-	pipelinesLabelPrefix = fmt.Sprintf("pipelines.%s", rhtapDomain)
+	pipelinesLabelPrefix = fmt.Sprintf("pipelines.%s", RhtapDomain)
 
 	// releaseLabelPrefix is the prefix of the release labels
-	releaseLabelPrefix = fmt.Sprintf("release.%s", rhtapDomain)
+	releaseLabelPrefix = fmt.Sprintf("release.%s", RhtapDomain)
 )
 
 // Labels to be used within Release PipelineRuns
 var (
 	// ApplicationNameLabel is the label used to specify the application associated with the PipelineRun
-	ApplicationNameLabel = fmt.Sprintf("%s/%s", rhtapDomain, "application")
+	ApplicationNameLabel = fmt.Sprintf("%s/%s", RhtapDomain, "application")
+
+	// ManagedCollectorsPipelineType is the value to be used in the PipelinesTypeLabel for managed collector Pipelines
+	ManagedCollectorsPipelineType = "managed-collectors"
+
+	// TenantCollectorsPipelineType is the value to be used in the PipelinesTypeLabel for tenant collector Pipelines
+	TenantCollectorsPipelineType = "tenant-collectors"
+
+	// FinalPipelineType is the value to be used in the PipelinesTypeLabel for final Pipelines
+	FinalPipelineType = "final"
 
 	// ManagedPipelineType is the value to be used in the PipelinesTypeLabel for managed Pipelines
 	ManagedPipelineType = "managed"
+
+	// TenantPipelineType is the value to be used in the PipelinesTypeLabel for tenant Pipelines
+	TenantPipelineType = "tenant"
 
 	// PipelinesTypeLabel is the label used to describe the type of pipeline
 	PipelinesTypeLabel = fmt.Sprintf("%s/%s", pipelinesLabelPrefix, "type")
@@ -72,5 +96,5 @@ var (
 	ReleaseNamespaceLabel = fmt.Sprintf("%s/%s", releaseLabelPrefix, "namespace")
 
 	// ReleaseSnapshotLabel is the label used to specify the snapshot associated with the PipelineRun
-	ReleaseSnapshotLabel = fmt.Sprintf("%s/%s", rhtapDomain, "snapshot")
+	ReleaseSnapshotLabel = fmt.Sprintf("%s/%s", RhtapDomain, "snapshot")
 )
