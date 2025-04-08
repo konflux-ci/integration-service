567cfe029916cede9f160d980209ee6c6143d60c
Ryan Cole <rcyoalne@gmail.com>
2025-04-08 11:10:02 -0400

    feat(stoneintg-1165): resolve pipelineruns from scenario
    Support referencing pipelineruns from integrationtestscenario resolver
    field.  Uses  Tekton's ResolutionRequest CR to grab remote data then
    creates PipelineRun in the cluster
    
    Signed-off-by: Ryan Cole <rcyoalne@gmail.com>

--- tekton/integration_pipeline_test.go
+++ tekton/integration_pipeline_test.go
@@ -103,8 +103,10 @@ var _ = Describe("Integration pipeline", func() {
 		Expect(k8sClient.Create(ctx, integrationTestScenarioGit)).Should(Succeed())
 
 		//create new integration pipeline run from integration test scenario
-		newIntegrationPipelineRun = tekton.NewIntegrationPipelineRun(prefix, namespace, *integrationTestScenarioGit)
+		newIntegrationPipelineRun, err := tekton.NewIntegrationPipelineRun(k8sClient, ctx, prefix, namespace, *integrationTestScenarioGit)
+		Expect(err).NotTo(HaveOccurred())
 		Expect(k8sClient.Create(ctx, newIntegrationPipelineRun.AsPipelineRun())).Should(Succeed())
+		Expect(newIntegrationPipelineRun).NotTo(BeNil())
 
 		integrationTestScenarioBundle = &v1beta2.IntegrationTestScenario{
 			ObjectMeta: metav1.ObjectMeta{
@@ -234,14 +236,16 @@ var _ = Describe("Integration pipeline", func() {
 		}
 		Expect(k8sClient.Create(ctx, hasComp)).Should(Succeed())
 
-		newIntegrationBundlePipelineRun = tekton.NewIntegrationPipelineRun(prefix, namespace, *integrationTestScenarioBundle).
-			WithIntegrationLabels(integrationTestScenarioBundle).
+		newIntegrationBundlePipelineRun, err = tekton.NewIntegrationPipelineRun(k8sClient, ctx, prefix, namespace, *integrationTestScenarioBundle)
+		Expect(err).NotTo(HaveOccurred())
+		newIntegrationBundlePipelineRun = newIntegrationBundlePipelineRun.WithIntegrationLabels(integrationTestScenarioBundle).
 			WithSnapshot(hasSnapshot).
 			WithApplication(hasApp)
 		Expect(k8sClient.Create(ctx, newIntegrationBundlePipelineRun.AsPipelineRun())).Should(Succeed())
 
-		enterpriseContractPipelineRun = tekton.NewIntegrationPipelineRun(prefix, namespace, *enterpriseContractTestScenario).
-			WithIntegrationLabels(enterpriseContractTestScenario).
+		enterpriseContractPipelineRun, err := tekton.NewIntegrationPipelineRun(k8sClient, ctx, prefix, namespace, *enterpriseContractTestScenario)
+		Expect(err).NotTo(HaveOccurred())
+		enterpriseContractPipelineRun = enterpriseContractPipelineRun.WithIntegrationLabels(enterpriseContractTestScenario).
 			WithIntegrationAnnotations(enterpriseContractTestScenario).
 			WithSnapshot(hasSnapshot).
 			WithExtraParams(enterpriseContractTestScenario.Spec.Params).
@@ -257,17 +261,26 @@ var _ = Describe("Integration pipeline", func() {
 		_ = k8sClient.Delete(ctx, integrationTestScenarioGit)
 		_ = k8sClient.Delete(ctx, integrationTestScenarioBundle)
 		_ = k8sClient.Delete(ctx, enterpriseContractTestScenario)
+		_ = k8sClient.Delete(ctx, newIntegrationPipelineRun.AsPipelineRun())
 		_ = k8sClient.Delete(ctx, newIntegrationPipelineRun)
 		_ = k8sClient.Delete(ctx, newIntegrationBundlePipelineRun)
 		_ = k8sClient.Delete(ctx, enterpriseContractPipelineRun)
 		_ = k8sClient.Delete(ctx, hasApp)
 		_ = k8sClient.Delete(ctx, hasSnapshot)
 		_ = k8sClient.Delete(ctx, hasComp)
-		_ = k8sClient.Delete(ctx, newIntegrationPipelineRun.AsPipelineRun())
 
 		os.Setenv("PIPELINE_TIMEOUT", "")
 		os.Setenv("TASKS_TIMEOUT", "")
 		os.Setenv("FINALLY_TIMEOUT", "")
+	})
+
+	Context("When creating a new IntegrationPipelineRun", func() {
+		FIt("can create an IntegrationPipelineRun", func() {
+			plr, err := tekton.NewIntegrationPipelineRun(k8sClient, ctx, prefix, namespace, *integrationTestScenarioGit)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(plr).NotTo(BeNil())
+			Expect(k8sClient.Create(ctx, plr.AsPipelineRun())).Should(Succeed())
+		})
 	})
 
 	Context("When managing a new IntegrationPipelineRun", func() {
