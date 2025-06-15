4793a2b9f2f69c6ecd4f497ac5959af3be976a2b
red-hat-konflux <126015336+red-hat-konflux[bot]@users.noreply.github.com>
2025-06-15 10:25:32 +0000

    fix(deps): update github.com/konflux-ci/application-api digest to 5a9670b
    Signed-off-by: red-hat-konflux <126015336+red-hat-konflux[bot]@users.noreply.github.com>

--- vendor/github.com/konflux-ci/application-api/api/v1alpha1/application_types.go
+++ vendor/github.com/konflux-ci/application-api/api/v1alpha1/application_types.go
@@ -84,7 +84,7 @@ type ApplicationStatus struct {
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
 
-// Application is the Schema for the applications API.  For a detailed description with examples, refer to <a href="https://github.com/redhat-appstudio/book/blob/main/book/HAS/hybrid-application-service-api.md"> Hybrid Application Service Kube API </a>
+// Application is the Schema for the applications API.  For description, refer to <a href="https://konflux-ci.dev/docs/reference/kube-apis/application-api/"> Hybrid Application Service Kube API </a>
 // +kubebuilder:resource:path=applications,shortName=hasapp;ha;app
 // +kubebuilder:subresource:status
 // +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
