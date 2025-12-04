398ecd56e789d154b54df0e4a138a3f9956f3c33
Hongwei Liu <hongliu@redhat.com>
2025-12-04 12:00:53 +0800

    fix(STONEINTG-1399): support running intg plr with plr res in PR
    * Previously we support running intg plr with pl resource in PR
    * The fix is to support running intg plr with plr resource in PR
    
    Signed-off-by: Hongwei Liu <hongliu@redhat.com>

--- internal/controller/snapshot/snapshot_suite_test.go
+++ internal/controller/snapshot/snapshot_suite_test.go
@@ -39,6 +39,7 @@ import (
 	applicationapiv1alpha1 "github.com/konflux-ci/application-api/api/v1alpha1"
 	releasev1alpha1 "github.com/konflux-ci/release-service/api/v1alpha1"
 	tektonv1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1"
+	resolutionv1beta1 "github.com/tektoncd/pipeline/pkg/apis/resolution/v1beta1"
 	clientsetscheme "k8s.io/client-go/kubernetes/scheme"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
@@ -95,6 +96,7 @@ var _ = BeforeSuite(func() {
 	Expect(tektonv1.AddToScheme(clientsetscheme.Scheme)).To(Succeed())
 	Expect(releasev1alpha1.AddToScheme(clientsetscheme.Scheme)).To(Succeed())
 	Expect(v1beta2.AddToScheme(clientsetscheme.Scheme)).To(Succeed())
+	Expect(resolutionv1beta1.AddToScheme(clientsetscheme.Scheme)).To(Succeed())
 
 	k8sManager, _ := ctrl.NewManager(cfg, ctrl.Options{
 		Scheme: clientsetscheme.Scheme,
