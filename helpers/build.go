99b0460a4df153acf8372eec425837703e3ae94e
dirgim <kpavic@redhat.com>
2026-08-07 18:13:38 +0200

    feat(STONEINTG-1744): support commit comment push triggers
    * Add IsPACPushStyleGitOpsComment which detects if an event
      is a commit comment as opposed to an issue comment on a PR
    * Treat build pipelineRuns and Snapshot which were created
      as a response to a commit comment as push-type Snapshots
      to align with PaC's triggering type
    * This is done by detecting the commit comment event type
      as well as checking if the target and source branches
      match (they do not match in case of PR/MR events)
    
    Signed-off-by: dirgim <kpavic@redhat.com>
    
    rh-pre-commit.version: 2.4.0
    rh-pre-commit.check-secrets: ENABLED

--- helpers/build.go
+++ helpers/build.go
@@ -1,7 +1,22 @@
+/*
+Copyright 2026 Red Hat Inc.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package helpers
 
 import (
 	"errors"
+	"strings"
 	"time"
 )
 
@@ -15,6 +30,19 @@ const (
 
 	// ChainsSignedCheckTimeout is how long after PLR completion we wait for Chains to sign before treating it as a failure
 	ChainsSignedCheckTimeout = 5 * time.Minute
+
+	// PAC GitOps comment event-type label values (from pipelines-as-code opscomments).
+
+	PipelineAsCodeTestCommentType      = "test-comment"
+	PipelineAsCodeTestAllCommentType   = "test-all-comment"
+	PipelineAsCodeRetestCommentType    = "retest-comment"
+	PipelineAsCodeRetestAllCommentType = "retest-all-comment"
+	PipelineAsCodeCancelCommentType    = "cancel-comment"
+	PipelineAsCodeCancelAllCommentType = "cancel-all-comment"
+	PipelineAsCodeOnCommentType        = "on-comment"
+
+	// GitRefBranchPrefix is the git prefix denoting a reference is a branch
+	GitRefBranchPrefix = "refs/heads/"
 )
 
 // ChainsNotSignedError indicates the PipelineRun is not yet signed by Chains.
@@ -31,4 +59,30 @@ func (e *ChainsNotSignedError) Error() string {
 func IsChainsNotSignedError(err error) bool {
 	var target *ChainsNotSignedError
 	return errors.As(err, &target)
+}
+
+// IsPACPushStyleGitOpsComment reports whether a PipelineRun looks like a
+// GitOps command on a pushed commit (commit_comment), as opposed to the same
+// command on a pull request (issue_comment).
+//
+// PAC rewrites EventType to ops strings for both paths, and may attach a
+// pull-request label to commit-comment runs when the SHA is linked to a PR.
+// Commit-comment handlers set HeadBranch == BaseBranch; PR handlers do not.
+func IsPACPushStyleGitOpsComment(eventType, targetBranch, sourceBranch string) bool {
+
+	if targetBranch == "" || sourceBranch == "" || strings.TrimPrefix(targetBranch, GitRefBranchPrefix) != strings.TrimPrefix(sourceBranch, GitRefBranchPrefix) {
+		return false
+	}
+	switch eventType {
+	case PipelineAsCodeTestCommentType,
+		PipelineAsCodeTestAllCommentType,
+		PipelineAsCodeRetestCommentType,
+		PipelineAsCodeRetestAllCommentType,
+		PipelineAsCodeCancelCommentType,
+		PipelineAsCodeCancelAllCommentType,
+		PipelineAsCodeOnCommentType:
+		return true
+	default:
+		return false
+	}
 }
