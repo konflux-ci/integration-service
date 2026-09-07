7c86e22ee59b7235ccf085729d05d718bd0f51de
dirgim <kpavic@redhat.com>
2026-09-07 18:07:53 +0200

    feat(STONEINTG-1796): limit scope of existing RBAC
    Scope down RBAC access in manager-role
    ClusterRole to least privilege
    
    Signed-off-by: dirgim <kpavic@redhat.com>

--- internal/controller/integrationpipeline/integrationpipeline_controller.go
+++ internal/controller/integrationpipeline/integrationpipeline_controller.go
@@ -60,7 +60,7 @@ func NewIntegrationReconciler(client client.Client, logger *logr.Logger, scheme 
 //+kubebuilder:rbac:groups=appstudio.redhat.com,resources=applications/finalizers,verbs=update
 //+kubebuilder:rbac:groups=appstudio.redhat.com,resources=applications,verbs=get;list;watch
 //+kubebuilder:rbac:groups=appstudio.redhat.com,resources=applications/status,verbs=get
-//+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch
+//+kubebuilder:rbac:groups="",resources=secrets,verbs=get
 //+kubebuilder:rbac:groups=pipelinesascode.tekton.dev,resources=repositories,verbs=get;list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
